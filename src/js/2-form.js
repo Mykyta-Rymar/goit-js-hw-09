19070892138243b1ce3031051ceabac6c54690f8
Mykyta_Rymar <mykitarymar@gmail.com>
2025-07-16 16:05:06 +0200

    ready

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,22 +7,22 @@ const form = document.querySelector('.feedback-form');
 const emailInput = form.elements.email;
 const messageTextarea = form.elements.message;
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', formSub);
+
+function formSub(event) {
   if (emailInput.value === '' || messageTextarea.value === '') {
+    event.preventDefault();
     alert('Fill please all fields');
+    return;
   } else {
     formData.email = emailInput.value.trim();
     formData.message = messageTextarea.value.trim();
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-    form.reset();
   }
-});
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   const allInfo = localStorage.getItem('feedback-form-state');
   const parInfo = JSON.parse(allInfo);
   console.log(parInfo);
-
-  form.reset();
-  localStorage.clear();
 });
